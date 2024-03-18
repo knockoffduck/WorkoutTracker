8ca76a1f402a1b0643a6b919f57ac6cf28af82b2
Daffa <ddarm4@gmail.com>
2024-03-19 01:21:15 +0800

    Implemented basic tab bar design

---
+++ FirstApp/Core/TabBar/TabBarButton.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarButton.swift
+//  FirstApp
+//
+//  Created by Daffa Fathurohman on 19/3/2024.
+//
+
+import SwiftUI
+
+struct TabBarButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBarButton()
+}
---
+++ FirstApp/Core/TabBar/TabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarView.swift
+//  FirstApp
+//
+//  Created by Daffa Fathurohman on 19/3/2024.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBarView()
+}
